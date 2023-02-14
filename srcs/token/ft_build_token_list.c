55c1098b3642a7821b2792f7c8e5aa30c8b77b0e
psydenst <psydenst.dev@gmail.com>
2023-02-14 17:37:00 -0300

    printf_list

--- srcs/token/ft_build_token_list.c
+++ srcs/token/ft_build_token_list.c
@@ -12,16 +12,35 @@ void	ft_build_token_list(t_shell *shell, char **input)
 	ft_free_arr((void **)input);
 }
 
+void printf_list(void *token)
+{
+	printf("%s\n", token);
+}
+
 static void	get_tokens(t_list *token_lst, char **cmd)
 {
 	int	i;
+	int j;
+	int k;
 
 	(void) token_lst;
+	j = 0;
 	i = 0;
 	while (cmd[i])
 	{
-		printf("token: %s\n", cmd[i]);
+		k = 0;
+		j = 0;
+		while (cmd[i][j])
+		{
+			if (cmd[i][j + 1] == ' ' || cmd[i][j + 1] == '>' || cmd[i][j + 1] == '<')
+			{
+				ft_lstadd_front(&token_lst, ft_lstnew(ft_substr(cmd[i], k, j - k)));
+				k = j + 1 + cmd[i][j + 1] == ' ';
+			}
+			j++;
+		}
 		i++;
 	}
+	ft_lstiter(token_lst, printf_list);
 	ft_free_arr((void **)cmd);
 }
